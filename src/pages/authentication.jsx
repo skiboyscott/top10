60a53d477412dae2f35d4c9d019407390b7a9aa2
mspeca <mspeca10@yahoo.com>
2025-07-23 11:53:03 -0500

    update

--- src/pages/authentication.jsx
+++ src/pages/authentication.jsx
@@ -5,7 +5,7 @@ import supabase from "../utils/supabaseClient";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
-const containerStyle = `bg-white rounded-2xl p-5 border border-gray-200/50 min-w-[375px] shadow-[0_25px_50px_-12px_rgba(0,0,0,0.45)]`
+const containerStyle = `bg-white rounded-2xl p-5 border border-gray-200/50 sm:min-w-[300px] md:min-w-[400px] shadow-[0_25px_50px_-12px_rgba(0,0,0,0.45)]`
 const pageStyle = "flex min-h-screen flex-col justify-start items-center sm:pt-2 md:pt-24"
 
 const Title = (props) => {
